c52dc5a34d2828da2a51e58e5a49d4bd5c7f60aa
mrTebrom <mr.tebron@gmail.com>
2024-06-12 17:13:40 +0500

    fix

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -6,62 +6,72 @@ import { CategoryService } from 'src/category/category.service';
 import { ProductAttribute } from 'src/attribute/product-attribute';
 import { Express } from 'express';
 import { Op } from 'sequelize';
+
 @Injectable()
 export class ProductService {
   constructor(
     @InjectModel(Product) private model: typeof Product,
-
-    @InjectModel(ProductAttribute)
-    private produtAttirubteModel: typeof ProductAttribute,
+    @InjectModel(ProductAttribute) private produtAttirubteModel: typeof ProductAttribute,
     private categoryService: CategoryService,
   ) { }
-  async create(dto: ProductCreateDto) {
-    const candidate = await this.model.findOne({ where: { title: dto.title } })
+
+  async create(dto: ProductCreateDto): Promise<Product> {
+    const candidate = await this.model.findOne({ where: { title: dto.title } });
     if (candidate) {
-      throw new HttpException('Данное название уже используется', HttpStatus.BAD_REQUEST)
+      throw new HttpException('Данное название уже используется', HttpStatus.BAD_REQUEST);
     }
+
     const { attributes } = dto;
+    console.log(attributes);
+
     const category = await this.categoryService.findById(dto.categoryId);
+    if (!category) {
+      throw new HttpException('Категория не найдена', HttpStatus.BAD_REQUEST);
+    }
+
     const product = await this.model.create({
       ...dto,
       categoryId: category.id,
-      images: dto.images.map(item => '/uploads/' + item)
+      images: dto.images.map(item => '/uploads/' + item),
     });
-    // Обновление значений атрибутов для конкретного товара
-    for (const attribute of attributes) {
-      const productAttribute = await this.produtAttirubteModel.findOne({
-        where: { productId: product.id, attributeId: attribute.id },
-      });
 
-      if (productAttribute) {
-        // Если запись существует, обновляем значение
-        productAttribute.value = attribute.value;
-        await productAttribute.save();
-      } else {
-        // Если запись не существует, создаем новую
-        await ProductAttribute.create({
-          productId: product.id,
-          attributeId: attribute.id,
-          value: attribute.value,
+    if (attributes && attributes.length > 0) {
+      for (const attribute of attributes) {
+        const productAttribute = await this.produtAttirubteModel.findOne({
+          where: { productId: product.id, attributeId: attribute.id },
         });
+
+        if (productAttribute) {
+          productAttribute.value = attribute.value;
+          await productAttribute.save();
+        } else {
+          await this.produtAttirubteModel.create({
+            productId: product.id,
+            attributeId: attribute.id,
+            value: attribute.value,
+          });
+        }
       }
     }
+
     return product;
   }
-  async findAll() {
+
+  async findAll(): Promise<Product[]> {
     return await this.model.findAll({ include: { all: true } });
   }
-  async findOne(id: number) {
+
+  async findOne(id: number): Promise<Product> {
     return await this.model.findByPk(id, { include: { all: true } });
   }
-  async update(id: number, dto: ProductUpdateDto) {
-    console.log(dto)
+
+  async update(id: number, dto: ProductUpdateDto): Promise<Product> {
+    console.log(dto);
     const product = await this.model.findByPk(id);
     if (!product) {
       throw new HttpException('Товар не найден', HttpStatus.NOT_FOUND);
     }
 
-    // Обновляем свойства товара, если они указаны в DTO
     if (dto.title) {
       product.title = dto.title;
     }
@@ -70,53 +80,53 @@ export class ProductService {
     }
     if (dto.categoryId) {
       const category = await this.categoryService.findById(dto.categoryId);
+      if (!category) {
+        throw new HttpException('Категория не найдена', HttpStatus.BAD_REQUEST);
+      }
       product.categoryId = category.id;
     }
     if (dto.description) {
       product.description = dto.description;
     }
-    // if (dto.images) {
-    //   // Предполагается, что переданные изображения в DTO уже содержат путь к изображению
-    //   product.images = dto.images;
-    // }
+    if (dto.images) {
+      product.images = dto.images.map(item => '/uploads/' + item);
+    }
 
-    // Сохраняем обновленный товар
     await product.save();
 
-    // Обновляем значения атрибутов для конкретного товара
     const { attributes } = dto;
-    for (const attribute of attributes) {
-      const productAttribute = await this.produtAttirubteModel.findOne({
-        where: { productId: product.id, attributeId: attribute.id },
-      });
-
-      if (productAttribute) {
-        // Если запись существует, обновляем значение
-        productAttribute.value = attribute.value;
-        await productAttribute.save();
-      } else {
-        // Если запись не существует, создаем новую
-        await ProductAttribute.create({
-          productId: product.id,
-          attributeId: attribute.id,
-          value: attribute.value,
+    if (attributes && attributes.length > 0) {
+      for (const attribute of attributes) {
+        const productAttribute = await this.produtAttirubteModel.findOne({
+          where: { productId: product.id, attributeId: attribute.id },
         });
+
+        if (productAttribute) {
+          productAttribute.value = attribute.value;
+          await productAttribute.save();
+        } else {
+          await this.produtAttirubteModel.create({
+            productId: product.id,
+            attributeId: attribute.id,
+            value: attribute.value,
+          });
+        }
       }
     }
 
     return product;
   }
-  async destroy(id: number) {
+
+  async destroy(id: number): Promise<void> {
     await this.model.destroy({ where: { id: id } });
   }
-  async uploadImages(images: Express.Multer.File[]) {
-    // Обработка загруженных изображений, сохранение в базу данных, возврат URL-адресов и т.д.
-    const imageUrls = images.map((image) => image.filename);
+
+  async uploadImages(images: Express.Multer.File[]): Promise<string[]> {
+    const imageUrls = images.map(image => '/uploads/' + image.filename);
     return imageUrls;
   }
 
-
-  async findNesTabs() {
-    return await this.model.findAll({ order: [['id', 'DESC']] })
+  async findNesTabs(): Promise<Product[]> {
+    return await this.model.findAll({ order: [['id', 'DESC']] });
   }
 }
